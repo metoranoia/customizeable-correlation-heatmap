b073977125dbdda7a3fbc551f13b711030840db5
metoranoia <88704945+metoranoia@users.noreply.github.com>
2022-08-07 20:42:04 +0700

    Add files via upload

--- correlation_plot.R
+++ correlation_plot.R
@@ -4,13 +4,15 @@ library(dplyr)
 library(knitr)
 library(tidyverse, warn.conflict=F)
 
+#import data
 LungCap_data <- read.csv("~/Learn R/bivariate analysis/LungCapData2.csv")
 str(LungCap_data)
 
+#change character data type to factor
 LungCap_data[sapply(LungCap_data, is.character)]=lapply(LungCap_data[sapply(LungCap_data, is.character)], as.factor)
 str(LungCap_data)
 summary(LungCap_data)
-
+sapply(LungCap_data[sapply(LungCap_data, is.factor)],levels)
 LungCap_data[sapply(LungCap_data, is.factor)]=lapply(LungCap_data[sapply(LungCap_data, is.factor)], as.numeric)
 
 lungcap_data_cor <- Hmisc::rcorr(as.matrix(LungCap_data))
@@ -26,51 +28,14 @@ cors <- function(df) {
 }
 cors(LungCap_data) %>% first() %>% head() %>% kable()
 
-cors(LungCap_data) %>%
-  map(~rownames_to_column(.x, var="measure1")) %>%
-  # look at the first element of the list (r)
-  first() %>%
-  head() %>%
-  kable()
-
-cors(LungCap_data) %>%
-  map(~rownames_to_column(.x, var="measure1")) %>%
-  # format each data set (r,P,n) long
-  map(~pivot_longer(.x, -measure1, "measure2")) %>%
-  # look at the first element of the list (r)
-  first() %>%
-  head() %>%
-  kable()
-
-cors(LungCap_data) %>%
-  map(~rownames_to_column(.x, var="measure1")) %>%
-  # format each data set (r,P,n) long
-  map(~pivot_longer(.x, -measure1, "measure2")) %>%
-  # merge our three list elements by binding the rows
-  bind_rows(.id = "id") %>%
-  head() %>%
-  kable()
-
-cors(LungCap_data) %>%
-  map(~rownames_to_column(.x, var="measure1")) %>%
-  # format each data set (r,P,n) long
-  map(~pivot_longer(.x, -measure1, "measure2")) %>%
-  # merge our three list elements by binding the rows
-  bind_rows(.id = "id") %>%
-  pivot_wider(names_from = id, values_from = value) %>%
-  # change so everything is lower case
-  rename(p = P) %>%
-  mutate(sig_p = ifelse(p < .05, T, F),
-         p_if_sig = ifelse(sig_p, p, NA),
-         r_if_sig = ifelse(sig_p, r, NA)) %>%
-  head() %>% 
-  kable()
-
 formatted_cors <- function(df){
   cors(df) %>%
     map(~rownames_to_column(.x, var="measure1")) %>%
+    # format each data set (r,P,n) long
     map(~pivot_longer(.x, -measure1, "measure2")) %>%
+    # merge our three list elements by binding the rows
     bind_rows(.id = "id") %>%
+    # merge our three list elements by binding the rows
     pivot_wider(names_from = id, values_from = value) %>%
     rename(p = P) %>%
     mutate(sig_p = ifelse(p < .05, T, F),
